a8c5617a1cc05148ebd0b6b92d663c143a6a5739
Sakar Mahat <sakarmahat@MacBook-Air.local>
2026-03-19 10:10:33 +1100

    DB models created

---
+++ models/users.py
@@ -0,0 +1,20 @@
+import uuid
+from sqlalchemy import String
+from sqlalchemy.orm import  Mapped, mapped_column, relationship
+
+# //local Imports
+from api.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[str] = mapped_column(
+        String(36),
+        primary_key=True,
+        default=lambda: str(uuid.uuid4())
+    )
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    labs: Mapped[list["Lab"]] = relationship("Lab", back_populates="owner") # type: ignore
+
