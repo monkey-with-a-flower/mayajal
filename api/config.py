a5b11276bd45728eb24096bf276915118eae2628
Sakar Mahat <sakarmahat@MacBook-Air.local>
2026-03-24 22:53:28 +1100

    Fixed DB import error finally

--- api/config.py
+++ api/config.py
@@ -2,8 +2,14 @@ from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DB_DIR = BASE_DIR / "db" / "mayajal.db"
+DB_DIR = BASE_DIR / "db" 
+DB_DIR.mkdir(parents=True, exist_ok=True)
 LAB_DIR = BASE_DIR / "labs"
+LAB_DIR.mkdir(parents=True, exist_ok=True)
 MACHINE_DIR = BASE_DIR / "machines"
+MACHINE_DIR.mkdir(parents=True, exist_ok=True)
 ASSETS_DIR = BASE_DIR / "assets"
+ASSETS_DIR.mkdir(parents=True, exist_ok=True)
+
+DB_PATH = DB_DIR / "mayajal.db"
 
