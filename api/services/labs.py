51d142d7ed65573217017b7f59b14b3686703fdb
Sakar Mahat <sakarmahat@macbook-air-1.tail57e798.ts.net>
2026-03-26 09:10:30 +1100

    Minor fixes

--- api/services/labs.py
+++ api/services/labs.py
@@ -1,17 +1,23 @@
+import asyncio
 from pathlib import Path
-from fastapi .responses import FileResponse
+from fastapi.responses import FileResponse,StreamingResponse
 from api.config import ASSETS_DIR, LAB_DIR, MACHINE_DIR
 from api.models.labs import Lab
 import subprocess
 
-def getPeerConfig(ownerID: str,labDir: Path):
-    config = Path(f"{labDir}/config/wireguard/peer_{ownerID}/peer_{ownerID}.conf")
+
+
+
+def getPeerConfig(lab:Lab):
+    labdir = Path(f"{LAB_DIR}/{lab.id}")
+    config = Path(f"{labdir}/config/wireguard/peer_peer1/peer_peer1.conf")
     return FileResponse(path=config, filename="peer_config.conf", media_type="application/octet-stream")
 
 
 
-def startLab(lab: Lab):
+async def startLab(lab: Lab):
     labdir = Path(f"{LAB_DIR}/{lab.id}")
+
     command = ["docker-compose","-f",f"{str(ASSETS_DIR)}/base_compose.yml"]
     
     for machine in lab.machines:
@@ -25,10 +31,17 @@ def startLab(lab: Lab):
     command.append("-d")
     try:
         print (command)
-        result = subprocess.run(command,capture_output=True, text=True,check=False)
-        return getPeerConfig(
-            lab.owner_id,labdir
-        )
+        result = asyncio.create_subprocess_exec(command,stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.STDOUT)
+        while True:
+            line = await result.stdout.readline()
+            if not line:
+                break
+            yield line.decode()
+
+        await result.wait()
+
+
     except subprocess.CalledProcessError as e:
         print("Error:", e.stderr)
     
